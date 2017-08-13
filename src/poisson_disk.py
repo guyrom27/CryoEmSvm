bed4cf217a74c2c7490ee4c6ce3b07e328c85584
guyrom27 <guy.rome@gmail.com>
2017-08-13 17:54:41 +0300

    dicrete poisson disk sample

--- src/poisson_disk.py
+++ src/poisson_disk.py
@@ -2,6 +2,7 @@ import scipy
 from random import random
 
 #reference: http://connor-johnson.com/2015/04/08/poisson-disk-sampling/
+#article: http://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
 class pds:
 
     def __init__(self, w, h, d, r, n, k=30):
@@ -18,6 +19,7 @@ class pds:
         self.w = w
         self.h = h
         self.d = d
+
         # n is the number of test points
         self.n = n
         #k is the number of attempts is a single sphere
@@ -25,11 +27,14 @@ class pds:
         self.r2 = r ** 2.0
         self.A = 3.0 * self.r2
         # cs is the cell size
-        self.cs = r / scipy.sqrt(2)
+        self.cs = r / scipy.sqrt(3)
         # gw and gh are the number of grid cells
         self.gw = int(scipy.ceil(self.w / self.cs))
         self.gh = int(scipy.ceil(self.h / self.cs))
         self.gd = int(scipy.ceil(self.h / self.cs))
+
+        self.bin_shape = [1, self.gw, self.gw * self.gh]
+
         # create a grid and a queue
         self.grid = [None] * self.gd * self.gw * self.gh
         self.queue = list()
@@ -38,9 +43,7 @@ class pds:
 
     def distance(self, x, y, z):
         # find where (x,y,z) sits in the grid
-        x_idx = int(x / self.cs)
-        y_idx = int(y / self.cs)
-        z_idx = int(z / self.cs)
+        [x_idx, y_idx, z_idx] = self.find_bin([x,y,z])
         # determine a neighborhood of cells around (x,y,z)
         x0 = max(x_idx - 2, 0)
         y0 = max(y_idx - 2, 0)
@@ -52,7 +55,7 @@ class pds:
         for z_idx in range(z0, z1):
             for y_idx in range(y0, y1):
                 for x_idx in range(x0, x1):
-                    step = (z_idx * self.gw * self.gh) + y_idx * self.gw + x_idx
+                    step = self.to_3d([x_idx,y_idx,z_idx])
                     # if the sample point exists on the grid
                     if self.grid[step]:
                         s = self.grid[step]
@@ -65,52 +68,75 @@ class pds:
                             return False
         return True
 
+    def find_bin(self, s):
+        return [int(x / self.cs) for x in s]
+
+    def to_3d(self, s):
+        return sum([x[0]*x[1] for x in zip(s,self.bin_shape)])
+
+
     def set_point(self, x, y, z):
         s = [x, y, z]
         self.queue.append(s)
-        # find where (x,y) sits in the grid
-        x_idx = int(x / self.cs)
-        y_idx = int(y / self.cs)
-        z_idx = int(x / self.cs)
-        step = (z_idx * self.gw * self.gh) + y_idx * self.gw + x_idx
-        self.grid[step] = s
         self.qs += 1
+
+        # find where (x,y) sits in the grid
+
+        step = self.to_3d(self.find_bin(s))
+
+        if (self.grid[step] != None):
+            print("hello")
+            assert(False)
+        self.grid[step] = s
         self.ss += 1
+
         return s
 
     def create_point_grid(self):
-        while self.qs < self.n:
+        while self.ss < self.n:
+            if (self.qs == 0):
+                print("randomization failed")
+                exit(1)
             idx_in_q = int(random() * self.qs)
             s = self.queue[idx_in_q]
             for i in range(self.k):
+
                 phi = 2 * scipy.pi * random()
                 theta = scipy.pi * random()
                 r = scipy.sqrt(self.A * random() + self.r2)
-                x = s[0] + r * scipy.sin(theta) * scipy.cos(phi)
-                y = s[1] + r * scipy.sin(theta) * scipy.sin(phi)
-                z = s[2] + r * scipy.cos(theta)
+
+                x = int(s[0] + r * scipy.sin(theta) * scipy.cos(phi))
+                y = int(s[1] + r * scipy.sin(theta) * scipy.sin(phi))
+                z = int(s[2] + r * scipy.cos(theta))
+
                 if (x >= 0) and (x < self.w):
                     if (y >= 0) and (y < self.h):
                         if (z >= 0) and (z < self.d):
                             if (self.distance(x, y, z)):
                                 self.set_point(x, y, z)
-                                if (self.qs == self.n + 1):
+                                if (self.ss >= self.n):
                                     break
             del self.queue[idx_in_q]
             self.qs -= 1
 
+
     def randomize_spaced_points(self):
         if self.ss == 0:
-            x = random() * self.w
-            y = random() * self.h
-            z = random() * self.d
+            [x,y,z] = [random() * i for i in [self.w, self.h, self.d]]
             self.set_point(x, y, z)
         self.create_point_grid()
         sample = list(filter(None, self.grid))
-        sample = scipy.asfarray(sample)
         return sample
 
 if __name__ == '__main__':
-    obj = pds(10, 20, 10, 2, 5)
-    sample1 = obj.randomize_spaced_points()
-    print(sample1)
+    import numpy as np
+    r = 2
+    n = 40
+    for i in range(100):
+        obj = pds(10, 20, 10, r, n)
+        sample1 = obj.randomize_spaced_points()
+        for i in enumerate(sample1):
+            for j in range(i[0]+1, len(sample1)):
+                if sum([(x[1]-x[0])**2 for x in zip(i[1],sample1[j])]) < r:
+                    assert(False)
+        assert(len(sample1) == n)
